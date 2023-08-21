b214d7016ca875ba0d4be50238dca8a019c2eee5
Sergey Dobrovolsky <misterdobrovolsky2017@gmail.com>
2023-08-21 22:24:50 +0300

    update

--- script.js
+++ script.js
@@ -1,77 +1,87 @@
 const getVarId = (id) => document.getElementById(id);
 const getVarClassName = (className) => document.querySelector(className);
-const people = [];
-let person = {};
-// Переменные 1-го слайда
-const inputLogin = getVarId('inputLogin');
-const inputPassword = getVarId('inputPassword');
-const buttonNext = getVarClassName('.carousel-control-next');
+
+
 const buttonCheck = getVarId('buttonCheck');
 
-// Переменные 2-го слайда
-const inputDate = getVarId('inputDate');
-const inputGender = getVarId('inputGender');
-const inputName = getVarId('inputName');
-const buttonCheck2 = getVarId('buttonCheck2');
+const classNames = [
+   'email',
+   'password',
+   'age',
+   'sex',
+   'name',
+   'zodiac',
+   'age',
+   'height',
+];
 
-// Переменные 3-го слайда
-const inputZodiac = getVarId('inputZodiac');
-const inputWeight = getVarId('inputWeight');
-const inputHeight = getVarId('inputHeight');
-const buttonCheck3 = getVarId('buttonCheck3');
+const generateNewPerson = () => {
+   const result = classNames.reduce((acc, item) => {
+      const value = getVarId(item).value;
+      if (value) {
+         acc[item] = value;
+      }
+      return acc;
+   }, {});
+   return result;
+};
+
+const checkFilledAllInputs = (arr) => (arr.find(elem => !getVarId(elem).value) ? alert('Enter all inputs!') : true);
 
 
+const showFinalSlide = () => {
+   const lastSlide = getVarId('lastSlide');
+   const lastIndicator = getVarId('lastIndicator')
+   lastSlide.insertAdjacentHTML('afterend',
+      `<div class="carousel-item">
+      <div class="d-block w-100">
+         <h3> <span style="color: aquamarine;">Ваши данные внесены</span> &#x1F609</h3>
+         <button id="buttonShowResult" type="submit">Показать всех участников</button>
+      </div>
+   </div>`)
+   lastIndicator.insertAdjacentHTML('afterend',
+      `<button type="button" id="resultIndicator" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3"
+   aria-label="Slide 4"></button>`)
+}
+
+const updateStorage = person => {
+   const storage = localStorage.getItem('people');
+   if (!storage) {
+      localStorage.setItem('people', JSON.stringify([person]));
+   } else {
+      const prevData = JSON.parse(localStorage.getItem('people'));
+      localStorage.setItem('people', JSON.stringify([...prevData, person]));
+   }
+};
+const showAllPeople = () => {
+   const data = JSON.parse(localStorage.getItem('people'));
+   let str = '';
+   for (let i of data) {
+      str += `${i.name} с логином ${i.email} <br>`
+   }
+   buttonShowResult.insertAdjacentHTML('afterend',
+      `<div style="text-align: left; color: #fff; font-size: 18px; margin-left: 100px;">${str}</div>`)
+}
+
+const addButtonDeleteStorage = () => {
+   buttonShowResult.insertAdjacentHTML('afterend',
+      `<button id="buttonDeleteAllPeople" type="submit">Удалить всех участников</button>`)
+}
 buttonCheck.addEventListener('click', function () {
-   if (inputLogin.value && inputPassword.value) {
-      buttonCheck.style.backgroundColor = '#80c780';
-      person = {
-         login: inputLogin.value,
-         password: inputPassword.value,
-      }
-      buttonNext.classList.toggle('disabled');
-      buttonNext.addEventListener('click', function () {
-         buttonNext.classList.toggle('disabled');
+
+   if (checkFilledAllInputs(classNames)) {
+      showFinalSlide()
+      const newPerson = generateNewPerson();
+      updateStorage(newPerson);
+      const buttonShowResult = getVarId('buttonShowResult');
+      buttonShowResult.addEventListener('click', () => {
+         showAllPeople();
+         addButtonDeleteStorage();
+         const buttonDeleteAllPeople = getVarId('buttonDeleteAllPeople')
+         buttonDeleteAllPeople.addEventListener('click',function () {
+            localStorage.removeItem('people');
+            location.reload()
+         });
       })
-   }
-   else {
-      buttonCheck.style.backgroundColor = 'red'
-   }
-})
-
-
-buttonCheck2.addEventListener('click', function () {
-   if (inputDate.value && inputGender.value && inputName.value) {
-      buttonCheck2.style.backgroundColor = '#80c780';
-      person = {
-         ...person,
-         data: inputDate.value,
-         gender: inputGender.value,
-         name: inputName.value,
-      }
-      buttonNext.classList.toggle('disabled');
-   } else {
-      buttonCheck2.style.backgroundColor = 'red'
-   }
-})
-buttonCheck3.addEventListener('click', function () {
-   if (inputZodiac.value && inputWeight.value && inputHeight.value) {
-      buttonCheck3.style.backgroundColor = '#80c780';
-      person = {
-         ...person,
-         zodiac: inputZodiac.value,
-         weight: inputWeight.value,
-         height: inputHeight.value,
-      }
-      people.push(person)
-      buttonNext.classList.toggle('disabled');
-      if (!!localStorage.getItem('people')) {
-         let peopleLocal = localStorage.getItem('people');
-         peopleLocal = [...JSON.parse(peopleLocal), ...people];
-         localStorage.setItem('people', JSON.stringify(peopleLocal))
-      } else {
-         localStorage.setItem('people', JSON.stringify(people))
-      }
-   } else {
-      buttonCheck3.style.backgroundColor = 'red'
    }
 })
