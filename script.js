712bb0e9c906f27b520d2e1cfe6267751867dfce
Sergey Dobrovolsky <misterdobrovolsky2017@gmail.com>
2023-09-12 17:00:14 +0300

    hw15

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
-// Рекурсія: Deep Copy
+const wrapper = document.querySelector('.wrapper');
 
-const deepCopy = (obj) => {
-   if (obj === null || typeof obj !== 'object') {
-      return obj; // Если obj - это null или не объект, вернем его как есть
+const fetchTitlesFromAPIs = async (arr, num) => {
+   let titles = '';
+   for (let i of arr) {
+      const response = await fetch(i);
+      const data = await response.json();
+      titles += `<li>${data.title}</li>`
    }
-
-   if (Array.isArray(obj)) {
-      // Если obj - это массив, создаем новый массив и рекурсивно копируем его элементы
-      const newArray = [];
-      for (let i = 0; i < obj.length; i++) {
-         newArray[i] = deepCopy(obj[i]);
-      }
-      return newArray;
-   }
-
-   // Если obj - это объект, создаем новый объект и рекурсивно копируем его свойства
-   const newObj = {};
-   for (const key in obj) {
-      if (obj.hasOwnProperty(key)) {
-         newObj[key] = deepCopy(obj[key]);
-      }
-   }
-   return newObj;
+   const listUl = document.querySelectorAll('.ul');
+   listUl[num].innerHTML = titles;
 }
+const makeNewCard = (arr) => {
+   for (let i of arr) {
+      wrapper.insertAdjacentHTML('beforeend',
+         `<div class="card">
+      <h3>${i.name}</h3>
+      <ul class="ul"></ul></div>`);
+   }
+
+}
+
+const fetchData = async () => {
+   const response = await fetch('https://swapi.dev/api/planets/')
+   const data = await response.json()
+      .then(data => data.results);
+   for (let i = 0; i < data.length; i++) {
+      fetchTitlesFromAPIs(data[i].films, i)
+   }
+   makeNewCard(data)
+}
+fetchData()
